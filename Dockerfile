64d2ee15697204111bd75279cc25d74e5c4e8773
Oller Perez <pablo.oller.perez@accenture.com>
2024-02-22 04:24:00 +0100

    primera implementacion del modelo

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM python:3.10.12
 # FROM openjdk:11-jdk
 RUN apt-get update 
 RUN apt-get install -y openjdk-17-jdk
-RUN pip install streamlit tabula-py tabulate
+RUN pip install streamlit scikit-learn tensorflow tabula-py tabulate
+RUN pip install joblib==1.1.0
+COPY model/* /app/model/
 COPY src/* /app/
 WORKDIR /app
 ENTRYPOINT [ "streamlit", "run", "app.py", "--server.port=8501", "--server.address=0.0.0.0"]
--- src/app.py
+++ src/app.py
@@ -1,9 +1,30 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import joblib
 from tabula.io import read_pdf
 
-st.title("FRAUD DETECTOR")
+
+modelo = joblib.load("model/modelo_fraud_detect.pkl")
+tablas_fraude = []
+
+st.markdown("""
+   <style>
+
+   .title {
+      text-align: center;
+      color: #FF4B4B;
+   }
+   .footer{
+      border: solid 1px #FF4B4B; 
+      text-align: center;
+      height: 100%;
+      background-color: #FF4B4B;
+      font-weight: bold;      
+   }
+   </style>
+""", unsafe_allow_html=True)
+st.markdown("<h1 class='title'>FRAUD DETECTOR</h1>", unsafe_allow_html=True)
 
 
 st.write("Vivimos en una sociedad en la que el fraude es el pan de cada día, desde engañar a personas mayores abusando de su desconocimiento de las nuevas tecnologías a intentar saltarse la ley de todas las maneras posibles para conseguir el mayor rédito posible. Con nuestra aplicación queremos ayudar a encontrar a los infractores de actividades tan inmorales y así poder llevarlos frente a la ley.")
@@ -26,11 +47,30 @@ if uploaded_files is not None:
                   st.write(f"Archivo: {uploaded_file.name}")
                   for i in range(len(tablas)-1):
                      st.write(f"Tabla {i+1}")
-                     st.table(tablas[i])
+                     # st.table(tablas[i])
             else:
                st.write("¡Solo se permiten un máximo de 5 archivos!")
                break
 
 
 
-st.markdown("Creadores: Pablo Oller Pérez, Pablo Santos Quirce, Alejandro Castillo Carmona")
+
+for tabla in tablas:
+   df = pd.DataFrame(tabla)
+
+   st.table(df)
+   fraudes = []   
+   # tablaCSV = df.to_csv("tablaCSV", index=False)
+  
+   for fila in df:
+      fraudes.append(modelo.predict(fila))
+   df['fraude'] = fraudes
+  
+   # Filtrar las filas donde 'fraude' es igual a 1
+   df_fraude = df[df['fraude'] == 1]
+
+   tablas_fraude.append(df_fraude)
+   st.table(df_fraude)
+
+
+st.markdown('<div class="footer">Creadores: Pablo Oller Pérez, Pablo Santos Quirce, Alejandro Castillo Carmona</div>', unsafe_allow_html=True)
