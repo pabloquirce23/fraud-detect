c5ce4434a9228d3e054eb30271e92677a4a59ed2
Pablo Santos Quirce <pabloquirce23@gmail.com>
2024-02-28 15:08:32 +0100

    cambios en app.py
    testeo de nueva estructura para el streamlit, tiene fallos, está sin comentar y falta estilizar

--- src/app.py
+++ src/app.py
@@ -1,76 +1,35 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import os
 import joblib
-from tabula.io import read_pdf
+import tabula
 
+@st.cache(allow_output_mutation=True)
+def load_model(model_file):
+    model = joblib.load(model_file)
+    return model
 
-modelo = joblib.load("model/modelo_fraud_detect.pkl")
-tablas_fraude = []
+def apply_models(df, clustering_fraud_detect, modelo_fraud_detect):
+   df['Cluster'] = clustering_fraud_detect.predict(df)
+   df['Fraud'] = modelo_fraud_detect.predict(df.drop(columns=['Cluster']))
+   return df
 
-st.markdown("""
-   <style>
+clustering_fraud_detect = load_model('clustering_fraud_detect.pkl')
+modelo_fraud_detect = load_model('modelo_fraud_detect.pkl')
 
-   .title {
-      text-align: center;
-      color: #FF4B4B;
-   }
-   .footer{
-      border: solid 1px #FF4B4B; 
-      text-align: center;
-      height: 100%;
-      background-color: #FF4B4B;
-      font-weight: bold;      
-   }
-   </style>
-""", unsafe_allow_html=True)
-st.markdown("<h1 class='title'>FRAUD DETECTOR</h1>", unsafe_allow_html=True)
+st.sidebar.title('Cargar archivos PDF')
+uploaded_files = st.sidebar.file_uploader('Cargar hasta 5 archivos PDF', accept_multiple_files=True)
 
+if uploaded_files:
+   for uploaded_file in uploaded_files:
+      st.subheader(f'Archivo PDF: {uploaded_file.name}')
 
-st.write("Vivimos en una sociedad en la que el fraude es el pan de cada día, desde engañar a personas mayores abusando de su desconocimiento de las nuevas tecnologías a intentar saltarse la ley de todas las maneras posibles para conseguir el mayor rédito posible. Con nuestra aplicación queremos ayudar a encontrar a los infractores de actividades tan inmorales y así poder llevarlos frente a la ley.")
+      try:
+         pdf_df = tabula.read_pdf(uploaded_file)
 
-files = []
-tablas = []
+         result_df = apply_models(pdf_df, clustering_fraud_detect, modelo_fraud_detect)
 
-uploaded_files = st.file_uploader("Inserta el archivo AQUÍ", accept_multiple_files=True)
+         st.write(result_df)
 
-if uploaded_files is not None:
-   if len(files) + len(uploaded_files) > 5:
-      st.write("¡Solo se permiten un máximo de 5 archivos!")
-   else:
-      for uploaded_file in uploaded_files:
-            if uploaded_file != None:
-               if len(files) < 5:
-                  files.append(uploaded_file)
-                  tablas = read_pdf(uploaded_file, pages = "all")
-                  tablas.append(tablas)
-                  st.write(f"Archivo: {uploaded_file.name}")
-                  for i in range(len(tablas)-1):
-                     st.write(f"Tabla {i+1}")
-                     # st.table(tablas[i])
-            else:
-               st.write("¡Solo se permiten un máximo de 5 archivos!")
-               break
-
-
-
-
-for tabla in tablas:
-   df = pd.DataFrame(tabla)
-
-   st.table(df)
-   fraudes = []   
-   # tablaCSV = df.to_csv("tablaCSV", index=False)
-  
-   for fila in df:
-      fraudes.append(modelo.predict(fila))
-   df['fraude'] = fraudes
-  
-   # Filtrar las filas donde 'fraude' es igual a 1
-   df_fraude = df[df['fraude'] == 1]
-
-   tablas_fraude.append(df_fraude)
-   st.table(df_fraude)
-
-
-st.markdown('<div class="footer">Creadores: Pablo Oller Pérez, Pablo Santos Quirce, Alejandro Castillo Carmona</div>', unsafe_allow_html=True)
+      except Exception as e:
+         st.error(f'Error al procesar el archivo {uploaded_file.name}: {e}')
