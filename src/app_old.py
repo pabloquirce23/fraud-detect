afff699a01e1e45622712b0df32bb127fb89a062
Pablo Santos Quirce <pabloquirce23@gmail.com>
2024-03-05 17:37:40 +0100

    recuperación de app.py antigua

--- src/app_old.py
+++ src/app_old.py
@@ -1,145 +1,256 @@
-import numpy as np
-import pandas as pd
 import streamlit as st
+import pandas as pd
+import tabula
+import joblib
+import tensorflow as tf
+import openai
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
-import shap
-import joblib
-from sklearn.preprocessing import StandardScaler
+import numpy as np
 
+# Configuración de la página
+st.set_page_config(
+    page_title="Fraud-Detect",
+    page_icon="💳",
+    initial_sidebar_state="expanded",
+  
+)
 
-df = pd.read_csv("data/creditcard.csv")
-df = df.sample(frac=0.01, random_state=42)
-
-# Estilos  ###################################################################################
-
-style_width = """
+# Estilo CSS para el centrado de la tabla
+st.write("""
     <style>
-        .appview-container  .main  .block-container{
-            max-width: 60%;
-        }
+    .st-emotion-cache-x1xsqf + .st-emotion-cache-x1xsqf + .st-emotion-cache-x1xsqf + .st-emotion-cache-x1xsqf + .st-emotion-cache-x1xsqf + .st-emotion-cache-x1xsqf + .st-emotion-cache-x1xsqf + .st-emotion-cache-x1xsqf + .st-emotion-cache-x1xsqf {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+}
     </style>
-    """
-st.markdown(style_width, unsafe_allow_html=True)
+""", unsafe_allow_html=True)
+# Estilo CSS para el título
+st.image("img/banner.webp", use_column_width=True, output_format='auto')
+st.markdown("""
+   <style>
+   .title {
+      text-align: center;
+      color: #1D3557;
+   }
+   
+   </style>
+""", unsafe_allow_html=True)
 
 
 
-# Cabecera  ##################################################################################
-st.title('Fraud Credit Card')
+# inicio de fraud detect
+st.markdown("<h1 class='title'>Fraud-Detect</h1>", unsafe_allow_html=True)
 
-st.markdown("""<br>""", unsafe_allow_html=True)
-st.write('Todos los datos sobre los empleados en una aplicación')
-# st.write(df)
 
-# Seleccionar las columnas V1 a V28 y mostrar las primeras 5 filas
-v_features = df.filter(regex='^V[0-9]+').head(60)
 
-# Mostrar las características seleccionadas
-st.dataframe(v_features, height=700)
+# Breve descripción de la aplicación
+st.write("**Fraud Detect** es una aplicación web diseñada para abordar de manera eficiente y precisa la detección de posibles fraudes bancarios. Su funcionalidad radica en la capacidad de procesar documentos en formato PDF, extrayendo las tablas contenidas en ellos mediante su lector integrado. A partir de los datos recopilados en estas tablas, la aplicación lleva a cabo un exhaustivo análisis para identificar posibles irregularidades financieras que puedan indicar la presencia de actividades fraudulentas entre una lista de clientes. Además muestra una sucesión de gráficas con datos que pueden ser de utilidad para el usuario.")
 
+# definición de columnas del dataset
+columnas = ['Time', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10',
+            'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 'V20',
+            'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28', 'Amount']
+
+# carga del modelo de detección de fraude
+modelo = joblib.load('model/modelo_fraud_detect.pkl')
+
+# carga del modelo de clustering
+modelo_clustering = joblib.load('model/clustering_fraud_detect.pkl')
+
+# creación del dataframe
+df = pd.DataFrame(columns=columnas)
+
+
+
+# boton de subida de archivos (no hay límite y se pueden eliminar en la propia página)
+uploaded_files = st.file_uploader("Elige tus archivos PDF", type="pdf", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    if uploaded_file is not None:
+        # lectura de las tablas de los archivos
+        df_temp_list = tabula.read_pdf(uploaded_file, pages='all')
+        for df_temp in df_temp_list:
+            # comprueba si es un dataframe
+            if isinstance(df_temp, pd.DataFrame):
+                # comprueba si tiene las columnas correctas
+                if set(columnas).issubset(df_temp.columns):
+                    # concatenación al dataframe principal
+                    df = pd.concat([df, df_temp], ignore_index=True)
+                else:
+                    st.write(f"El archivo {uploaded_file.name} no tiene las columnas correctas.")
+            else:
+                # shn kgm
+                st.write(f"El archivo {uploaded_file.name} no contiene ninguna tabla.")
+
+# reemplaza las comas por puntos para poder convertir en float
+df = df.replace(',', '.', regex=True)
+
+# conversión de los datos de los archivos a float
+df = df.astype(float)
+
+# cálculo de la media para poder aplicar el modelo de clustering
+columnas_median = ['V1', 'V2', 'V3', 'V4', 'V5', 'V6', 'V7', 'V8', 'V9', 'V10', 
+                   'V11', 'V12', 'V13', 'V14', 'V15', 'V16', 'V17', 'V18', 'V19', 
+                   'V20', 'V21', 'V22', 'V23', 'V24', 'V25', 'V26', 'V27', 'V28']
+
+df['Median'] = df[columnas_median].mean(axis=1)
+
+# columnas necesarias para la aplicación del modelo de clustering
+df_clustering = df[['Median', 'Amount']]
+
+# conversión de dataframe a tensor
+df_tensor = tf.convert_to_tensor(df[columnas].values, dtype=tf.float32)  # Solo selecciona las columnas originales aquí
+
+df_clustering_tensor = tf.convert_to_tensor(df_clustering.values, dtype=tf.float32)
+
+st.divider()
+
+# aplicación de los modelos
 if not df.empty:
-    st.divider()
-    with st.container():
-        class_counts = df['Class'].value_counts()
-        # Estableciendo el estilo de Seaborn
-        sns.set_style("whitegrid")
+   # Switcher para mostrar o no las gráficas
+   on = st.toggle('Mostrar las gráficas')
+   df['Class'] = modelo.predict(df_tensor)
+   df['Cluster'] = modelo_clustering.predict(df_clustering_tensor)
 
-        # Creando el gráfico de barras con Seaborn
-        # plt.figure(figsize=(8, 5))
-        # sns.barplot(x=class_counts.index, y=class_counts.values, palette="Blues_d")
+   # creación del dataframe para guardar los resultados
+   results_df = pd.DataFrame(columns=["Detección Fraude", "Cluster"])
 
-        # plt.title('Distribución de Clases (0: Legítimo, 1: Fraude)', fontsize=16)
-        # plt.xlabel('Clase', fontsize=14)
-        # plt.ylabel('Número de Instancias', fontsize=14)
-        # plt.xticks(ticks=[0, 1], labels=['Legítimo', 'Fraude'], fontsize=12)
-        # ticks = [1, 10, 100, 1000, 10000, 100000]
-        # plt.yticks(ticks, labels=[str(tick) for tick in ticks])
-        # # plt.yticks(fontsize=12)
-        # plt.yscale('log')
-        # st.pyplot(plt.gcf())
-        # plt.close()
-        # Crear el gráfico de barras
-        fig = px.bar(x=class_counts.index, y=class_counts.values, log_y=True, labels={'x': 'Clase', 'y': 'Número de Instancias'},
-                    title='Distribución de Clases (0: Legítimo, 1: Fraude) con Escala Logarítmica')
+   # diccionario para definir las etiquetas de la columna cluster
+   cluster_labels = {0: "PG", 1: "LS", 2: "BN", 3: "LN", 4: "IN"}
 
-        # Mejorar la presentación
-        fig.update_layout(
-            xaxis_tickmode='array',
-            xaxis_tickvals=[0, 1],
-            xaxis_ticktext=['Legítimo', 'Fraude'],
-            # Ajustes de tamaño
-            height=600,
-            title_x=0.25,
-            title_font=dict(size=22)
-        )
+   # bucle para añadir los resultados al dataframe
+   for i in range(len(df)):
+       new_row = pd.DataFrame({"Detección Fraude": ["NO FRAUDE ✅" if df['Class'][i] == 0 else "FRAUDE ❌"], 
+                               "Cluster": [cluster_labels[df['Cluster'][i]]]})
+       results_df = pd.concat([results_df, new_row], ignore_index=True)
 
-        # Añadir anotación para explicar la escala logarítmica
-        fig.add_annotation(
-            x=0.5, y=-0.2,
-            xref="paper", yref="paper",
-            text="Escala logarítmica: cada paso aumenta diez veces",
-            showarrow=False,
-            font=dict(size=12),
-            align="center"
-        )
-        
-        # Para asegurar que la anotación sea visible, ajusta el margen inferior
-        fig.update_layout(margin=dict(b=120))  # Ajusta el valor según sea necesario
-        
-        # Mostrar el gráfico
-        st.plotly_chart(fig, use_container_width=True)
+   st.dataframe(results_df)
 
+   st.divider()
 
-    st.divider()
-    with st.container():
-        # Suponiendo que 'data' es tu DataFrame y ya lo has cargado con pd.read_csv
-        # Asegúrate de incluir todas las columnas relevantes, aquí asumimos que 'Class' está incluida
+   # gráfica que relaciona la predicción con los clusters (sorted para que salgan de menor a mayor)
+   clusters = sorted(df['Cluster'].unique())
 
-        # Calculando la matriz de correlación
-        correlation_matrix = df.astype('float32').corr()
+   # diccionario para mapear los números del cluster a las etiquetas preferidas
+   cluster_labels_2 = {0: "PG", 1: "LS", 2: "BN", 3: "LN", 4: "IN"}
 
-        # Creando el mapa de calor
-        plt.figure(figsize=(18, 16))
-        sns.heatmap(correlation_matrix, annot=True, fmt=".2f", cmap='coolwarm', cbar=True)
-        plt.title('Matriz de Correlación de Características')
-        plt.xticks(rotation=45)
-        plt.yticks(rotation=45)
-        st.pyplot(plt.gcf())
-        plt.close()
+   fig, axs = plt.subplots(1, len(clusters), figsize=(10, 15))
+
+   # definición de los colores para "Fraud" y "Not Fraud"
+   colors = ['darkblue', 'lightblue']
+
+   # se crea una gráfica circular por cada cluster
+   for i, cluster in enumerate(clusters):
+       # filtra el dataframe al cluster pertinente
+       df_cluster = df[df['Cluster'] == cluster]
+
+       # conteo de casos de fraude
+       fraud_counts = df_cluster['Class'].value_counts()
+
+       # cálculo del porcentaje de fraud y not fraud
+       not_fraud_percentage = fraud_counts.get(0, 0) / fraud_counts.sum() * 100
+       fraud_percentage = 100 - not_fraud_percentage
+
+       # creación de las etiquetas
+       labels = [f'Fraud {fraud_percentage:.1f}%', f'Not Fraud {not_fraud_percentage:.1f}%']
+
+       # creación de la gráfica
+       axs[i].pie([fraud_percentage, not_fraud_percentage], startangle=90, colors = colors)
+
+       # alternación de la posición de título y leyenda
+       if i % 2 == 0:
+           axs[i].set_title(cluster_labels_2[cluster], y=1.1)
+           axs[i].legend(labels, loc='upper center', bbox_to_anchor=(0.5, -0.05), fancybox=True, shadow=True, ncol=5)
+       else:
+           axs[i].set_title(cluster_labels_2[cluster], y=-0.1)
+           axs[i].legend(labels, loc='lower center', bbox_to_anchor=(0.5, 1.05), fancybox=True, shadow=True, ncol=5)
+   
+   # Si se enciende se activa la gráfica
+   if on:
+      st.pyplot(fig)
+      st.divider()
 
 
-    st.divider()
-    with st.container():
-        # Crear una gráfica de caja para cada característica
-        plt.figure(figsize=(16, 10))
-        sns.boxplot(data=df, orient='h', palette='Set2')
-        plt.title('Distribución de Características')
+   # Crea un gráfico de dispersión para visualizar los clusters
+   plt.figure(figsize=(10, 6))
 
-        # Seleccionar un subconjunto de características de interés
-        features_of_interest = ['Time', 'Amount'] + ['V%d' % i for i in range(1, 5)]  # Ejemplo: 'Time', 'Amount', 'V1', 'V2', 'V3', 'V4'
+   for cluster in df['Cluster'].unique():
 
-        # Filtrar el DataFrame para incluir solo esas características
-        df_subset = df[features_of_interest]
+       # Filtra los datos por cluster
+       cluster_data = df[df['Cluster'] == cluster]
 
-        # Ahora, crear la gráfica de caja para este subconjunto de datos
-        plt.figure(figsize=(16, 10))
-        sns.boxplot(data=df_subset, orient='h', palette='Set2')
-        plt.title('Distribución de Características Seleccionadas')
+       # Plotea los datos con un color diferente para cada cluster
+       plt.scatter(cluster_data['Median'], cluster_data['Amount'], label=f'{cluster_labels_2[cluster]}')
+
+   plt.title('Distribución de Transacciones por Clusters')
+   plt.xlabel('Mediana de V1-V28')
+   plt.ylabel('Amount')
+   plt.legend()
+    
+   # Si se enciende se activa la gráfica
+   if on:
+      st.pyplot(plt)
+      st.divider()
+
+else:
+   st.write('No se han subido archivos PDF válidos.')
 
 
-        # Seleccionar características numéricas para estandarizar
-        # features = df.select_dtypes(include=['float64', 'int64']).columns.tolist()
+with st.sidebar:
+    # inicio del chatbot
+    st.markdown("<h1 class='title'>Eto'o Bot</h1>", unsafe_allow_html=True)
 
-        # Estandarizar las características
-        # scaler = StandardScaler()
-        # df_scaled = pd.DataFrame(scaler.fit_transform(df[features]), columns=features)
+    # api key de openai
+    openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-        # Crear boxplots para un subconjunto de características estandarizadas
-        # plt.figure(figsize=(16, 10))
-        # sns.boxplot(data=df_scaled[['Time', 'Amount']] + df_scaled.filter(regex='^V[0-9]+').columns.tolist(), orient='h', palette='Set2')
-        # plt.title('Distribución de Características Estandarizadas')
-        
-        
-        st.pyplot(plt.gcf())
-        plt.close()
+    # selección del modelo con el que queremos trabajar
+    if "openai_model" not in st.session_state:
+        # 00475-AEDF-52510-2
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+    # inicialización del histórico del chat
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # mostrar los mensajes del histórico
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # reacción al input del usuario
+    if prompt := st.chat_input("Escriba aquí su consulta"):
+        # muestra el mensaje del usuario en su contenetendor de mensaje
+        with st.chat_message("user"):
+            st.markdown(prompt)
+        # añade el mensaje del usuario al histórico
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        with st.chat_message("assistant"):
+            # placeholder vacío
+            message_placeholder = st.empty()
+            full_response = ""
+            # llamada de la api de openai
+            for response in openai.ChatCompletion.create(
+                # se pasa el modelo y el histórico
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                # este parámetro hace que vaya escribiendo poco a poco la respuesta
+                stream=True,
+            ):
+                # se añade una parte de la respuesta del chatbot en cada iteración del bucle
+                full_response += response.choices[0].delta.get("content", "")
+                # enseña lo que hay de respuesta
+                message_placeholder.markdown(full_response + "| ")
+            message_placeholder.markdown(full_response)
+        # añadido de la respuesta al histórico
+        st.session_state.messages.append({"role": "assistant", "content": full_response})
+
+# Pie de página con información de los creadores
+st.markdown('Creadores:')
+st.page_link("https://www.linkedin.com/in/pablo-oller-perez-7995721b2", label="**Pablo Oller Pérez**")
+st.page_link("https://github.com/acscr44", label="**Pablo Santos Quirce**")
+st.page_link("https://github.com/pabloquirce23", label="**Alejandro Castillo Carmona**")
